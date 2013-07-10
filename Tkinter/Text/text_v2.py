61d50a975559d4732a3349cfc648be996affc7e8
Hunter Thornsberry <hunter@hunterthornsberry.com>
2013-07-10 18:23:49 -0400

    Cleaning up

--- Tkinter/Text/text_v2.py
+++ Tkinter/Text/text_v2.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 
 root = Tk()
-root.title("Write To File")
+root.title("Create A File")
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -13,27 +13,33 @@ mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
 
-file_name = "HAB"
+file_name = StringVar()
 
-filename = ttk.Entry(mainframe, width=50, textvariable=(file_name))
-filename.grid(column=2, row=1, sticky=(N, W))
+file_loc = ttk.Entry(mainframe, width=50, textvariable=(file_name))
+file_loc.grid(column=2, row=1, sticky=(N, W))
 
-target = open(file_name, "w")
+def create(*args):
+	global target
+	target = open(str(file_name), "w+")
 
 def write(*args):
-	#target.open(filename, "w")
+	#target.open(file_name, "w")
 	target.write(text_write.get())
+	#target.write(file_name.get())
 	target.close()
 
 def open_file(*args):
 	os.system("open " + str(file_name))
 
-
+#Labels
 ttk.Label(mainframe, text="Filename").grid(column=1, row=1, sticky=(E))
-ttk.Label(mainframe, text="Type your text here").grid(column=1, row=2, sticky=(E))
-ttk.Button(mainframe, text="Write", command=write).grid(column=3, row=4, sticky=W)
-ttk.Label(mainframe, text="Hunter Thornsberry").grid(column=2, row=5, sticky=S)
-ttk.Button(mainframe, text="Open", command=open_file).grid(column=3, row=5, sticky=W)
+ttk.Label(mainframe, text="File Text").grid(column=1, row=2, sticky=(E))
+ttk.Label(mainframe, text="Created by Hunter Thornsberry").grid(column=2, row=3, sticky=S)
+
+#Buttons
+ttk.Button(mainframe, text="Create", command=create).grid(column=3, row=1, sticky=W)
+ttk.Button(mainframe, text="Write", command=write).grid(column=3, row=2, sticky=W)
+ttk.Button(mainframe, text="Open", command=open_file).grid(column=3, row=3, sticky=W)
 
 text_to_write = StringVar()
 
